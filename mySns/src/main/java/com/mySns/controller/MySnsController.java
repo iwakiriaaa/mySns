eea26d05e86ee459de86cb22c5eb90e1437f1867
yIwakiri1 <y-iwakiri@any-plus.co.jp>
2023-04-26 16:43:52 +0900

    コンフリクト2

--- mySns/src/main/java/com/mySns/controller/MySnsController.java
+++ mySns/src/main/java/com/mySns/controller/MySnsController.java
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.mySns.model.LoginIdSession;
 
 import jakarta.servlet.http.HttpSession;
-//aaaaaaaaaaaaaaaaaaaaaaaa
-//aaaaaaaaaaaaaaaaaaaaaaaa
 @Controller
 public class MySnsController {
 
